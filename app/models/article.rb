392ad8dae58b2ab1c4da3e4e26af05d5011c00cd
viphat <viphat@gmail.com>
2014-08-08 20:43:56 +0700

    dwdw

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,17 @@
 class Article < ActiveRecord::Base
 	#them
-	validates :title, :content, :presence => true
-	validates :published_at_in_future
+	validates :title, :content, :published_at, :presence => true #custom validation
+	validate :published_at_in_future,:title_longer_than_10
+	def self.class_method_here
+		
+	end
 	def published_at_in_future
-		if selg.published_at > Date.today
-			self.errors.add(:published_at, "Published at should be")
-			
+		if self.published_at && self.published_at > Date.today 
+			self.errors.add(:published_at, " should be in future")
 		end
+	end
+	def title_longer_than_10
+		if self.title && self.title.length < 10
+			self.errors.add(:title," should longer than 10 char")
 	end
 end
